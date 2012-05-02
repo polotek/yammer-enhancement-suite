4737413be52022c08ac6115a876dc983735a9480
Collin VanDyck <collinvandyck@gmail.com>
2012-05-02 11:47:56 -0700

    Save as draft seems to be working, kind of.

--- scripts/saveasdraft.js
+++ scripts/saveasdraft.js
@@ -2,16 +2,38 @@ $(function() {
 
 	var networkId = $(".uni-current-network.yj-clearfix").data("yj-network-id")
 
-	function textareaId(el) {
-		return networkId + ":" + $(el).parents("form").data("yamjs-id")
+	function draftStorageKey(el) {
+		return "drafts_" + networkId + ":" + $(el).parents(".yj-message-form-container").find("form").data("yamjs-id")
 	}
 
+	function keyPressHandler(event) {
+		var target = event.target
+		var draftKey = draftStorageKey(target)
+		localStorage[draftKey] = $(target).val()
+		console.log(draftKey + ": " + $(target).val())
+	}
+
+	$("body").on("click", "a.yj-message-form-submit", function(event) {
+		var target = event.target
+		var draftKey = draftStorageKey(target)
+		localStorage.removeItem(draftKey)
+	})
+
 	$("body").on("focus", "textarea", function() {
-		console.log("focused " + textareaId(this))
+		var draftKey = draftStorageKey(this)
+		console.log("focused " + draftKey)
+		var saved = localStorage[draftKey]
+		if (saved) {
+			$(this).val(saved)
+		}
+		$(this).on("keyup", keyPressHandler)
+
 	})
 
 	$("body").on("blur", "textarea", function() {
-		console.log("blur " + textareaId(this))
+		var draftKey = draftStorageKey(this)
+		console.log("blur " + draftKey)
+		$(this).off("keyup", keyPressHandler)
 	})
 
 })
