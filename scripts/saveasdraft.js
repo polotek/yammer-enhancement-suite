c5b8c29cef255fe279193de83ce588af44cd79e8
Collin VanDyck <collinvandyck@gmail.com>
2012-05-02 10:43:36 -0700

    Use network and textarea ids

--- scripts/saveasdraft.js
+++ scripts/saveasdraft.js
@@ -1,3 +1,17 @@
 $(function() {
-	console.log(yammerEnhance)
+
+	var networkId = $(".uni-current-network.yj-clearfix").data("yj-network-id")
+
+	function textareaId(el) {
+		return networkId + ":" + $(el).parents("form").data("yamjs-id")
+	}
+
+	$("body").on("focus", "textarea", function() {
+		console.log("focused " + textareaId(this))
+	})
+
+	$("body").on("blur", "textarea", function() {
+		console.log("blur " + textareaId(this))
+	})
+
 })
