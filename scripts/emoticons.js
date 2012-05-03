bc4117f9674ad5e27465a66fa0b7de423c513261
Matt Shannon <dmshannon@gmail.com>
2012-05-02 23:50:53 -0700

    Added sprites to emoticons, fiddled with pinned header spacing

--- scripts/emoticons.js
+++ scripts/emoticons.js
@@ -1,41 +1,49 @@
 (function($) {
 
-	// these will be converted to something like
-	// .emote-%s	
-	var sprites = [
-		{ token: ":-)", class: "smiley" },
-		{ token: ":)", 	class: "smiley" },
-		{ token: ":-(", class: "sad" },
-		{ token: ":(", 	class: "sad" }
-	],
-	fixToken = function(token) {
-		var replacement = token.replace(")", "\\)").replace("(", "\\(");
-		console.log("changing token", token, " to ", replacement);
-		return replacement;
-	};
+  // these will be converted to something like
+  // .emote-%s  
+  var sprites = [
+    { token: ":-)",     class: "smiley" },
+    { token: ":)",      class: "smiley" },
+    { token: ":-(",     class: "sad"    },
+    { token: ":(",      class: "sad"    },
+    { token: ":D",      class: "teethy" },
+    { token: ":-D",     class: "teethy" },
+    { token: ":poop:",  class: "poop"   },
+    { token: ":-X",     class: "nervous"},
+    { token: ":X",      class: "nervous"},
+    { token: ":x",      class: "nervous"},
+    { token: "&lt;3",      class: "heart"  },
+    { token: "&lt;/3",    class: "brokenheart" }
+  ],
+  fixToken = function(token) {
+    return token.replace(")", "\\)").replace("(", "\\(");
+  };
 
-	$(function() { 
+  $(function() { 
 
-		for(var i = 0, ii = sprites.length; i < ii; i++) {
-			sprites[i]['regex'] = new RegExp(fixToken(sprites[i].token), "g");
-			sprites[i]['html'] = "<span class=\"enhance-emote enhance-emote" + sprites[i].class + "\" /></span>";
-		}
+    for(var i = 0, ii = sprites.length; i < ii; i++) {
+      sprites[i]['regex'] = new RegExp(fixToken(sprites[i].token), "g");
+      sprites[i]['html'] = "<span class=\"enhance-emote enhance-emote" + sprites[i].class + "\" alt=\"" + sprites[i].class + "\" /></span>";
+    }
 
-		/* quick and dirty woot */
-		setInterval(function() { 
-			$('span.yj-message:not(.smiled)').each(function() {
-				var self = $(this).addClass('smiled'), 
-					text = self.html(),
-					originalLength = text.length;
+    /* quick and dirty woot */
+    setInterval(function() { 
+      $('span.yj-message:not(.smiled)').each(function() {
+        var self = $(this).addClass('smiled'), 
+          text = self.html(),
+          originalLength = text.length;
 
-				for(var i = 0, ii = sprites.length; i < ii; i++) {
-					text = text.replace(sprites[i].regex, sprites[i].html);
-				}
+        console.log("inspecting ", text);
 
-				if(originalLength !== text.length)
-					self.html(text);
-			});
-		}, 1000)
-	})
+        for(var i = 0, ii = sprites.length; i < ii; i++) {
+          text = text.replace(sprites[i].regex, sprites[i].html);
+        }
+
+        if(originalLength !== text.length)
+          self.html(text);
+      });
+    }, 1000)
+  })
 
 })(jQuery);
