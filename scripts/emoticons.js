1a43b8b25456516878a5857792373c0f3560c7e1
Marco Rogers <marco.rogers@gmail.com>
2012-05-29 00:21:31 -0700

    refactor emoticons

--- scripts/emoticons.js
+++ scripts/emoticons.js
@@ -1,53 +1,65 @@
 (function($) {
-
-  // these will be converted to something like
-  // .emote-%s  
-  var sprites = [
-    { token: "=)",      class: "smiley" },
-    { token: ":-)",     class: "smiley" },
-    { token: ":)",      class: "smiley" },
-    { token: ":-(",     class: "sad"    },
-    { token: ":(",      class: "sad"    },
-    { token: ":D",      class: "teethy" },
-    { token: ":-D",     class: "teethy" },
-    { token: ":poop:",  class: "poop"   },
-    { token: ":-X",     class: "nervous"},
-    { token: ":X",      class: "nervous"},
-    { token: ":x",      class: "nervous"},
-    { token: "&lt;3",   class: "heart"  },
-    { token: "&lt;/3",  class: "brokenheart" },
-    { token: ":P",      class: "goofy"  },
-    { token: ":-P",     class: "goofy"  },
-    { token: "=P",      class: "goofy"  },
-  ],
-  fixToken = function(token) {
-    return token.replace(")", "\\)").replace("(", "\\(");
-  };
-
-  $(function() { 
-
-    for(var i = 0, ii = sprites.length; i < ii; i++) {
-      sprites[i]['regex'] = new RegExp(fixToken(sprites[i].token), "g");
-      sprites[i]['html'] = "<span class=\"enhance-emote enhance-emote" + sprites[i].class + "\" alt=\"" + sprites[i].class + "\" /></span>";
+  var emoticons = {
+    // these will be converted to something like
+    // .emote-%s  
+    sprites: [
+      { token: "=)",      class: "smiley" },
+      { token: ":-)",     class: "smiley" },
+      { token: ":)",      class: "smiley" },
+      { token: ":-(",     class: "sad"    },
+      { token: ":(",      class: "sad"    },
+      { token: ":D",      class: "teethy" },
+      { token: ":-D",     class: "teethy" },
+      { token: ":poop:",  class: "poop"   },
+      { token: ":-X",     class: "nervous"},
+      { token: ":X",      class: "nervous"},
+      { token: ":x",      class: "nervous"},
+      { token: "&lt;3",   class: "heart"  },
+      { token: "&lt;/3",  class: "brokenheart" },
+      { token: ":P",      class: "goofy"  },
+      { token: ":-P",     class: "goofy"  },
+      { token: "=P",      class: "goofy"  },
+    ]
+    , _fixToken: function(token) {
+      return token.replace(")", "\\)").replace("(", "\\(");
     }
+    , _initSprites: function() {
+      var sprite;
+      for(var i = 0, ii = this.sprites.length; i < ii; i++) {
+        sprite = this.sprites[i];
+        sprite.regex = new RegExp(this._fixToken(sprite.token), "g");
+        sprite.html = "<span class=\"enhance-emote enhance-emote" + sprite.class + "\" alt=\"" + sprite.class + "\" /></span>";
+      }
+    }
+    , enhanceEmoticons: function() {
+      var self = this;
+      $('span.yj-message:not(.smiled)').each(function(idx, el) {
+        var $el = $(el).addClass('smiled') 
+          , sprite
+          , text = $el.html()
+          , originalLength = text.length;
 
-    /* quick and dirty woot */
-    setInterval(function() { 
-      $('span.yj-message:not(.smiled)').each(function() {
-        var self = $(this).addClass('smiled'), 
-          text = self.html(),
-          originalLength = text.length;
-
-        console.log("inspecting ", text);
-
-        for(var i = 0, ii = sprites.length; i < ii; i++) {
-          text = text.replace(sprites[i].regex, sprites[i].html);
+        for(var i = 0, len = self.sprites.length; i < len; i++) {
+          sprite = self.sprites[i];
+          text = text.replace(sprite.regex, sprite.html);
         }
 
-        if(originalLength !== text.length)
-          self.html(text);
+        if(originalLength !== text.length) {
+          $el.html(text);
+        }
       });
-    }, 250)
-  })
+    }
+    , init: function() {
+      var self = this;
 
+      $(function() {
+        self._initSprites();
+
+        setInterval(self.enhanceEmoticons, 250);
+      });
+    }
+  };
+
+  utils.bindAll(emoticons);
+  emoticons.init();
 })(jQuery);
