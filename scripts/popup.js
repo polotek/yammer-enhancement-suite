878c1c2699eb8c5cd44ce1064f6fa0a7916a822f
Marco Rogers <marco.rogers@gmail.com>
2012-05-28 23:40:32 -0700

    popup initializes properly and saves settings

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,26 @@
 var yes = utils.getYES();
-$('#settings').find(':input').change(function(ev) {
-  var $el = $(ev.target);
-  yes.set($el.attr('id'), $el.val());
-});
+yes.popup = {
+  $inputs: $('#settings').find(':input')
+  , init: function() {
+    this.$inputs.each(function(i, input) {
+      var $input = $(input);
+      if($input.attr('type') === 'checkbox') {
+        $input.attr('checked', yes.get($input.attr('id')));
+      } else {
+        $input.val(yes.get($input.attr('id')));
+      }
+    });
+
+    this.$inputs.change(this.onSettingsChange);
+  }
+  , onSettingsChange: function(ev) {
+    var $input = $(ev.target);
+    if($input.attr('type') === 'checkbox') {
+      yes.set($input.attr('id'), $input.attr('checked'));
+    } else {
+      yes.set($input.attr('id'), $input.val());
+    }
+  }
+};
+utils.bindAll(yes.popup);
+yes.popup.init();
